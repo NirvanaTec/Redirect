80e9ce46c33d5a589927914c8b11c5d9f126e0ff
NirvanaTec <fengheng1111@126.com>
2024-12-09 16:46:25 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 # NirvanaRedirect
 
-#### 介绍
+## 介绍
 涅槃科技官方重定向中转开源，让您的跳转不在一样，非同一般。
 
-#### 演示地址
+## 使用说明
+修改[NPyyds.js](js%2FNPyyds.js)中的<br>---------------------------------<br>
+title 标题内容<br>
+imgSrc 图片链接<br>
+cssHref 背景地址<br>
+url 跳转链接【可选】<br>
+----------涅---槃--科--技--------------
+
+## 使用介绍
+1. 在HTML中自动嵌入加载动画<br>
+2. 加载动画后跳转至其它链接达到中转
+
+## 演示地址
 https://npyyds.top/
 
-#### 赞助微信
-![](img/wx.png) 
-#### 赞助支付宝
-![](img/zfb.png) 
+## 赞助微信
+<img src="img/wx.png" alt="微信二维码" width="40%" />
 
-#### 开源来源
+## 赞助支付宝
+<img src="img/zfb.png" alt="支付宝二维码" width="40%" />
+
+## 开源来源
 https://npyyds.top/<br>
 https://gitee.com/newNP/Redirect/<br>
 https://github.com/NirvanaTec/Redirect/<br>
--- js/NPyyds.js
+++ js/NPyyds.js
@@ -1,10 +1,97 @@
+/**
+ * 图片链接
+ * 效果 <img src = "img/NPy60.png">
+ */
+const imgSrc = "img/NPy60.png";
+
+
+/**
+ * 标题内容
+ * 加载时图片下面的标题
+ */
+const title = "涅槃科技";
+
+
+/**
+ * 跳转链接【可选】
+ * 如果是空值【null】就会触发隐藏加载模式
+ */
+const url = "https://npyyds.top/";
+
+
+/**
+ * 背景地址
+ * 效果 <link rel="stylesheet" href="css/NPyyds.css">
+ */
+const cssHref = "css/NPyyds.css";
+
+
+// 不建议修改后面的配置 请遵守开源许可
 console.log("-------------------------");
 console.log("涅槃科技 © 版权所有");
 console.log("One: npyyds.top");
 console.log("Two: buy.npyyds.top");
 console.log("点击链接加入腾讯频道【涅槃科技】: https://pd.qq.com/s/7s4bjpv06");
 console.log("项目开源地址: https://gitee.com/newNP/");
+console.log("项目开源地址: https://github.com/NirvanaTec/");
 console.log("-------------------------");
 console.log("");
 console.log("如果你使用此项目，请不要删除上面的信息！");
 console.log("最终解释权归涅槃科技所有！");
+
+window.onload = function () {
+
+    const elements = document.querySelectorAll('*'); // 所有元素
+    const old = []; // 备份所有可见
+
+    if (url == null) {
+        elements.forEach(element => {
+            old.push(element.style.display);  // 备份可见
+            if (element.tagName !== "HTML" && element.tagName !== "HEAD" && element.tagName !== "BODY") {
+                element.style.display = "none";
+            }
+        });
+    }
+
+    const css = document.createElement("link"); // 背景css引用
+    css.rel = "stylesheet";
+    css.href = cssHref;
+    document.body.appendChild(css); // 背景css引用
+
+    const nirvana = document.createElement("div"); // 图片和标题分组
+    nirvana.className = "Nirvana";
+
+    const nirvanaimg = document.createElement("img"); // 图片
+    nirvanaimg.src = imgSrc;
+
+    const nirvanatitle = document.createElement("p"); // 标题
+    nirvanatitle.textContent = title;
+
+    nirvana.appendChild(nirvanaimg); // 图片
+    nirvana.appendChild(nirvanatitle); // 标题
+
+    const space = document.createElement("div"); // 高斯模糊
+    space.className = "NirvanaSpace";
+
+    const background = document.createElement("div"); // 背景图
+    background.className = "Nirvanabackground";
+
+    document.body.appendChild(nirvana); // 图片和标题
+    document.body.appendChild(space); // 高斯模糊
+    document.body.appendChild(background); // 背景图
+
+    // 复原初始状态
+    setTimeout(() => {
+        if (url == null) {
+            document.body.removeChild(nirvana); // 图片和标题
+            document.body.removeChild(space); // 高斯模糊
+            document.body.removeChild(background); // 背景图
+            document.body.removeChild(css); // 背景css引用
+            elements.forEach((element, index) => {
+                element.style.display = old[index];  // 恢复原始可见
+            });
+        } else {
+            window.location.href = url;
+        }
+    }, 2300);
+};
